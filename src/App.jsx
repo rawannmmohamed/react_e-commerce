078b8d993713b4d234e11dd2f2197b6d9704296c
rawannmmohamed <rawanmelkhodary@gmail.com>
2024-06-30 20:24:02 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Nav from './components/Nav'
+import Slider from './components/Slider'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Nav/>
+     <Slider/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Slider.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Slider.css";
+const Slider = () => {
+  return <div>slider</div>;
+};
+
+export default Slider;
