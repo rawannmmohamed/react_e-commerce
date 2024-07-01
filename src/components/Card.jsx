5e054e669d3717a61973e841a590bfdba416570b
rawannmmohamed <rawanmelkhodary@gmail.com>
2024-07-01 06:34:38 +0300

    slider component fetching data & styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Card.css";
-const Card = () => {
+const Card = (slide) => {
   return (
-    <div className="card">
+    <div className="card" style={{ backgroundImage: `url(${slide.image})` }}>
       <div className="description">
         <div className="info">
-          <span className="title">camera</span>
-          <span className="price">$97.70</span>
+          <span className="title">{slide.name}</span>
+          <span className="price">{slide.price}</span>
         </div>
         <button className="card-button">VIEW</button>
       </div>
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Slider.css";
-import Card from "./Card"
+import Card from "./Card";
+
 const Slider = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/slides.json');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="slider">
-    <Card/>
+      <ul className="slides">
+        {data.map((card, index) => (
+          <Card key={index} name={card.name} price={card.price} image={card.image} />
+        ))}
+      </ul>
     </div>
   );
 };
